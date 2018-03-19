76bac5609814827c3a62e62bac5f45c6c8d9e5e5
yss <yss@yss-PC>
2018-03-19 18:37:03 +0800

    修改

--- src/test/java/authorizer/BitAndWildPermissionResolver.java
+++ src/test/java/authorizer/BitAndWildPermissionResolver.java
@@ -11,40 +11,9 @@ public class BitAndWildPermissionResolver implements Permission {
 	private int permissionBit;
 	private String instanceId;
 
-	public void  BitPermission(String permissionString) {
-	        String[] array = permissionString.split("\\+");
-	        if(array.length > 1) {
-	            resourceIdentify = array[1];
-	        }
-	        if(StringUtils.isEmpty(resourceIdentify)) {
-	            resourceIdentify = "*";
-	        }
-	        if(array.length > 2) {
-	            permissionBit = Integer.valueOf(array[2]);
-	        }
-	        if(array.length > 3) {
-	            instanceId = array[3];
-	        }
-	        if(StringUtils.isEmpty(instanceId)) {
-	            instanceId = "*";
-	        }
-	    }
-
-	public boolean implies(Permission p) {
-		if (!(p instanceof BitPermission)) {
-			return false;
-		}
-		BitPermission other = (BitPermission) p;
-		if (!("*".equals(this.resourceIdentify) || this.resourceIdentify.equals(other.resourceIdentify))) {
-			return false;
-		}
-		if (!(this.permissionBit == 0 || (this.permissionBit & other.permissionBit) != 0)) {
-			return false;
-		}
-		if (!("*".equals(this.instanceId) || this.instanceId.equals(other.instanceId))) {
-			return false;
-		}
-		return true;
+	public boolean implies(Permission arg0) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
